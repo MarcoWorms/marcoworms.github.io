0aa4259cb592b0af2908d59a2a14dcce0c9bca21
Marco Guaspari Worms <marcogworms@gmail.com>
2015-09-30 23:16:59 -0300

    crafting system

--- mss/itemFactory.js
+++ mss/itemFactory.js
@@ -15,10 +15,37 @@ function GatheringItem() {
 }
 GatheringItem.prototype = new Item();
 
+function Pickaxe() {
+    this.itemType = "pickaxe";
+}
+Pickaxe.prototype = new GatheringItem();
+
+function Gloves() {
+    this.itemType = "gloves";
+}
+Gloves.prototype = new GatheringItem();
+
+
+
+function BadPickaxe() {
+    this.name = "Bad Pickaxe"
+    this.gatheringRate = 1600
+    this.action = function(player, rng) {
+
+        if (rng <= 60000) {
+            player.ores["Copper"] += 1;
+        }
+
+        if (rng >= 40000) {
+            player.ores["Tin"] += 1;
+        }
+    }
+}
+BadPickaxe.prototype = new Pickaxe();
 
 function BronzePickaxe() {
 	this.name = "Bronze Pickaxe"
-    this.gatheringRate = 1500
+    this.gatheringRate = 1400
 	this.action = function(player, rng) {
 
 		if (rng <= 70000) {
@@ -34,7 +61,46 @@ function BronzePickaxe() {
 		}
 	}
 }
-BronzePickaxe.prototype = new GatheringItem();
+BronzePickaxe.prototype = new Pickaxe();
+
+function IronPickaxe() {
+    this.name = "Iron Pickaxe"
+    this.gatheringRate = 1200
+    this.action = function(player, rng) {
+
+        if (rng <= 90000) {
+            player.ores["Copper"] += 1;
+        }
+
+        if (rng >= 10000) {
+            player.ores["Tin"] += 1;
+        }
+
+        if (rng <= 40000) {
+            player.ores["Iron"] += 1;
+        }
+    }
+}
+IronPickaxe.prototype = new Pickaxe();
+
+
+function BadGloves() {
+    this.name = "Bad Gloves"
+    this.gatheringRate = 3000
+    this.action = function(player, rng) {
+
+        if (rng <= 30000) {
+            if (player.ores["Copper"] >= 10 && player.ores["Tin"] >= 10) {
+                player.ores["Copper"] -= 10;
+                player.ores["Tin"] -= 10;
+                player.bars["Bronze"] += 1;
+            }
+
+        }
+
+    }
+}
+BadGloves.prototype = new Gloves();
 
 function BronzeGloves() {
 	this.name = "Bronze Gloves"
@@ -60,7 +126,7 @@ function BronzeGloves() {
 
 	}
 }
-BronzeGloves.prototype = new GatheringItem();
+BronzeGloves.prototype = new Gloves();
 
 
 
--- mss/main.js
+++ mss/main.js
@@ -2,6 +2,36 @@
 
     game = this;
 
+    var craftingItems = {
+        "BronzePick" : {
+            "instance" : new itemFactory.BronzePickaxe(),
+            "active" : true,
+            "clickable" : false,
+            "crafted" : false,
+            "recipe" : {
+                "Bronze" : 2
+            }
+        },
+        "BronzeGloves" : {
+            "instance" : new itemFactory.BronzeGloves(),
+            "active" : true,
+            "clickable" : false,
+            "crafted" : false,
+            "recipe" : {
+                "Bronze" : 2
+            }
+        },
+        "IronPick" : {
+            "instance" : new itemFactory.IronPickaxe(),
+            "active" : false,
+            "clickable" : false,
+            "crafted" : false,
+            "recipe" : {
+                "Iron" : 10
+            }
+        }
+    }
+
 	var player = {
 		"ores" : {
 			"Copper" : 0,
@@ -13,7 +43,7 @@
 			"Runite" : 0
 		},
 		"bars" : {
-			"Bronze" : 0,
+			"Bronze" : 2,
 			"Iron" : 0,
 			"Steel" : 0,
 			"Mithril" : 0,
@@ -21,48 +51,45 @@
 			"Runite" : 0
 
 		},
-		"monsters" : {
-
-		},
-		"pickaxe" : new itemFactory.BronzePickaxe(),
-		"gloves" : new itemFactory.BronzeGloves()
+		"pickaxe" : new itemFactory.BadPickaxe(),
+		"gloves" : new itemFactory.BadGloves()
 	}
 
-    function mine(player, count, ticks) {
+    function mine(count, ticks) {
 
         document.getElementById("mineProg").value = count;
 
         count += 1/ticks
 
         if (count < 1) {
-            window.setTimeout(function(){mine(player, count, ticks);}, player.pickaxe.gatheringRate/ticks)
+            window.setTimeout(function(){mine(count, ticks);}, player.pickaxe.gatheringRate/ticks)
         } else {
             player.pickaxe.activate(player);
-            mine(player, 0, ticks);
-            updateGui(player);
+            mine(0, ticks);
+            updateGui();
 
         }
 
 	}
 
-	function smith(player, count, ticks) {
+	function smith(count, ticks) {
 
         document.getElementById("smithProg").value = count
 
         count += 1/ticks
 
         if (count < 1) {
-            window.setTimeout(function(){smith(player, count, ticks);}, player.gloves.gatheringRate/ticks)
+            window.setTimeout(function(){smith(count, ticks);}, player.gloves.gatheringRate/ticks)
         } else {
             player.gloves.activate(player);
-            smith(player, 0, ticks);
-            updateGui(player);
+            smith(0, ticks);
+            updateGui();
         }
 
     }
 
 
-	function updateGui(player) {
+	function updateGui() {
 
 		//Prints current pickaxe
 		document.getElementById("pickaxeName").innerHTML = player.pickaxe.name;
@@ -71,13 +98,66 @@
 
 
 
+        var craftingList = ""
+        document.getElementById("craftingList").innerHTML = craftingList;
+        for (item in craftingItems) {
+            if (craftingItems[item]["crafted"]) {
+                continue;
+            };
+            var recipe = craftingItems[item]["recipe"];
+            var canMake = true;
+            for (material in recipe) {
+                var barAmmount = player["bars"][material];
+                var recipeAmmount = recipe[material];
+
+                if (barAmmount < recipeAmmount || barAmmount === undefined) {
+                    craftingItems[item]["clickable"] = false
+                } else {
+                    craftingItems[item]["clickable"] = true
+                    craftingItems[item]["active"] = true;
+                }
+            }
+            if (craftingItems[item]["active"]) {
+                var id = item;
+                var name = craftingItems[item]["instance"].name;
+                var recipe = JSON.stringify(craftingItems[item]["recipe"])
+
+                craftingList += "<b>" + name + "</b> -\
+                                <span>" + recipe + "</span>\
+                                <button id='" + id + "'>Craft</button><br>";
+
+            }
+        }
+        document.getElementById("craftingList").innerHTML = craftingList;
+
+        for (item in craftingItems) {
+            if (craftingItems[item]["crafted"]) {
+                continue;
+            };
+            if (craftingItems[item]["clickable"] && craftingItems[item]["active"]) {
+                var id = item;
+                document.getElementById(id).addEventListener("click", function(e) {
+                    var item = e.currentTarget.id;
+                    player[craftingItems[item]["instance"].itemType] = craftingItems[item]["instance"];
+
+                    for (material in craftingItems[item]["recipe"]) {
+                        player["bars"][material] -= craftingItems[item]["recipe"][material];
+                        console.log(player["bars"][material])
+                    }
+
+                    craftingItems[item]["crafted"] = true
+
+                    updateGui();
+                });
+            }
+        }
 
 
 		//Prints ore list
 		var oresList = ""
 		for (ore in player.ores) {
-			ammount = player.ores[ore]
-			if (ammount === 0) {continue;} // doesn't prit ores you don't have
+			var ammount = player.ores[ore]
+			if (ammount === 0) {continue;} // doesn't print ores you don't have
 			oresList += ore + ": " + ammount + "<br>"
 		}
 		document.getElementById("oresList").innerHTML = oresList;
@@ -85,8 +165,8 @@
 		//Prints bar list
 		var barsList = ""
 		for (bar in player.bars) {
-			ammount = player.bars[bar]
-			if (ammount === 0) {continue;} // doesn't prit bars you don't have
+			var ammount = player.bars[bar]
+			if (ammount === 0) {continue;} // doesn't print bars you don't have
 			barsList += bar + ": " + ammount + "<br>"
 		}
 		document.getElementById("barsList").innerHTML = barsList;
@@ -96,7 +176,7 @@
 
 	updateGui(player);
 
-    mine(player, 0, 20);
-    smith(player, 0, 20);
+    mine(0, 40);
+    smith(0, 40);
 
 })();
