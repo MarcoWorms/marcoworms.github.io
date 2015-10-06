324a0300cbc476c4057de5111c6cf90eb4a4e850
Marco Guaspari Worms <marcogworms@gmail.com>
2015-10-06 15:00:57 -0300

    fixing atlas

--- megamen/gameStates/load.js
+++ megamen/gameStates/load.js
@@ -6,6 +6,7 @@ var loadState = {
         var loadingLabel = game.add.text(80, 150, "Loading...", {font: "30px Arial", fill: "#ffffff"});
 
         game.load.atlas('megamen', 'assets/atlas.png', 'assets/atlas.json', Phaser.Loader.TEXTURE_ATLAS_JSON_ARRAY);
+        console.log("Atlas loaded")
     },
 
     create : function() {
