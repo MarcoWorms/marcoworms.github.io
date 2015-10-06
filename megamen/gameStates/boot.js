725d21659ec32a57c18e72caa9fdd00a0d7118fb
Marco Guaspari Worms <minhoca111@gmail.com>
2015-10-06 12:35:16 +0000

    fixing jslint

--- megamen/gameStates/boot.js
+++ megamen/gameStates/boot.js
@@ -1,9 +1,10 @@
 var bootState = {
     create : function() {
         console.log("Booting...")
-        
+        /*global game*/ 
+        /*global Phaser*/ 
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        game.state.start('load')
+        game.state.start('load');
     }
 }
--- megamen/main.js
+++ megamen/main.js
@@ -1,5 +1,7 @@
+/*global Phaser*/
 var game = new Phaser.Game(800, 600, Phaser.AUTO, "gameDiv");
 
+/*global bootState*/
 game.state.add('boot', bootState);
 //game.state.add('load', loadState);
 //game.state.add('menu', menuState);
