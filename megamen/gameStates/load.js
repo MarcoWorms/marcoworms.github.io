073db04bf3713d8ecfa64ea18ef6d4454e60223c
Marco Guaspari Worms <marcogworms@gmail.com>
2015-10-06 14:45:12 -0300

    making spritesheet atlas

--- megamen/gameStates/load.js
+++ megamen/gameStates/load.js
@@ -4,13 +4,14 @@ var loadState = {
     preload: function() {
 
         var loadingLabel = game.add.text(80, 150, "Loading...", {font: "30px Arial", fill: "#ffffff"});
-        
+
         //spritesheet needs fixing
         game.load.spritesheet("megamen", "assets/spritesheet.jpg");
+        game.load.atlas('megamen', 'assets/atlas.png', 'assets/atlas.json', Phaser.Loader.TEXTURE_ATLAS_JSON_ARRAY);
     },
-    
+
     create : function() {
         game.state.start("welcome");
     }
-    
+
 }
