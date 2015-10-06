11901c1706ad16cd6f0c6f2176ff481d44e9fd23
Marco Guaspari Worms <minhoca111@gmail.com>
2015-10-06 13:14:37 +0000

    need to fix spritesheet

--- megamen/gameStates/load.js
+++ megamen/gameStates/load.js
@@ -5,8 +5,8 @@ var loadState = {
 
         var loadingLabel = game.add.text(80, 150, "Loading...",{font: "30px Arial", fill: "#ffffff"});
         
-        //I will load the spritesheet here
-        //game.load.image("player", "assets/player")
+        //spritesheet needs fixing
+        game.load.spritesheet("megamen", "spritesheet.jpg")
     },
     
     create : function() {
