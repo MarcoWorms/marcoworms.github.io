610669c9562d1e17889fb95a7f220550b54e5811
Marco Guaspari Worms <marcogworms@gmail.com>
2015-10-07 12:46:16 -0300

    fixed state flow

--- megamen/gameStates/menu.js
+++ megamen/gameStates/menu.js
@@ -1,56 +1,5 @@
 var menuState = {
     create: function() {
-
-        game.stage.backgroundColor = "#005C34";
-
-        this.cursors = game.input.keyboard.createCursorKeys();
-        this.jumpButton = this.cursors.up;
-
-        this.map = this.game.add.tilemap('tilemap');
-        this.map.addTilesetImage('tileset', 'tileset');
-        this.bglLayer = this.map.createLayer('bgLayer');
-        this.levelLayer = this.map.createLayer('level1Layer');
-        this.dieLayer = this.map.createLayer('die1Layer');
-
-        this.map.setCollisionBetween(1, 500, true, 'level1Layer');
-        this.map.setCollisionBetween(1, 500, true, 'die1Layer');
-
-        this.levelLayer.resizeWorld();
-
-        this.player = new Player(100, 10);
-        game.physics.arcade.gravity.y = 700;
-
-        var tutoLabel = game.add.text(80, 50, "<- e -> para andar, ^ para pular",{font: "25px Arial", fill: "#ffffff"});
-    },
-    update: function() {
-
-        game.physics.arcade.collide(this.player.sprite, this.levelLayer);
-        game.physics.arcade.collide(this.player.sprite, this.dieLayer, die, null, this);
-
-        if (this.player.isJumping && this.player.sprite.body.onFloor()) {
-           this.player.isJumping = false;
-           this.player.state = "finished jumping"
-        }
-
-        if (this.cursors.right.isDown) {
-            this.player.moveX(220);
-        } else if (this.cursors.left.isDown) {
-            this.player.moveX(-220);
-        } else {
-            this.player.moveX(0);
-        }
-
-        if (this.jumpButton.isDown) {
-            this.player.jump();
-        }
-
-        function die() {
-            game.state.start("menu")
-        }
-
-    },
-    render: function() {
-        game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
-        //game.debug.body(this.player.sprite);
+        game.state.start("play");
     }
 }
--- megamen/main.js
+++ megamen/main.js
@@ -12,5 +12,6 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('welcome', welcomeState);
 game.state.add('menu', menuState);
+game.state.add('play', playState);
 
 game.state.start('boot');
