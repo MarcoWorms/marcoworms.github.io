0625f2fd299d829f5889d28f42049db6cf6ff9c8
Marco Guaspari Worms <marcogworms@gmail.com>
2015-10-26 22:52:52 -0200

    physics playground

---
+++ playground/main.js
@@ -0,0 +1,68 @@
+function Simulation(width, height) {
+
+    this.canvas = document.getElementById('simulation');
+    this.canvas.width = width
+    this.canvas.height = height
+    this.context = this.canvas.getContext('2d');
+
+    this.loop = undefined;
+    this.timeLastFrame = new Date().getTime();
+
+    this.start = function() {
+        var sphereWithGravity = new SphereWithGravity();
+        this.objects = [sphereWithGravity];
+
+        window.cancelAnimationFrame(this.loop);
+        this.mainLoop();
+    }
+
+    this.stop = function() {
+        window.cancelAnimationFrame(this.loop);
+        this.clearCanvas();
+    }
+
+    this.mainLoop = function() {
+        this.loop = window.requestAnimationFrame(this.mainLoop.bind(this));
+        var timeNow = new Date().getTime();
+        var deltaTime = 0.001*(timeNow - this.timeLastFrame); // in seconds
+        this.timeLastFrame = timeNow;
+        this.updateObjects(deltaTime);
+    }
+
+    this.updateObjects = function(deltaTime) {
+        for (var i = this.objects.length - 1; i >= 0; i--) {
+            this.objects[i].preUpdate(deltaTime);
+            this.objects[i].update(deltaTime);
+        };
+        this.drawObjects();
+    }
+    this.drawObjects = function() {
+        this.clearCanvas();
+        for (var i = this.objects.length - 1; i >= 0; i--) {
+            this.objects[i].draw(this.context);
+        };
+    }
+
+    this.clearCanvas = function() {
+        this.context.fillStyle = 'white';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+}
+
+window.onload = function() {
+
+    var simulation = new Simulation(800, 600);
+    simulation.start();
+
+    var startButton = document.getElementById('start');
+    startButton.onclick = function() {
+        simulation.start();
+    }
+
+    var stopButton = document.getElementById('stop');
+    stopButton.onclick = function() {
+        simulation.stop();
+    }
+
+}
---
+++ playground/objects.js
@@ -0,0 +1,43 @@
+function Object() {
+    this.x = 100;
+    this.y = 100;
+    this.m = 1;
+    this.vx = 2;
+    this.vy = 1;
+
+    this.preUpdate = function(deltaTime) {
+
+    }
+    this.update = function(deltaTime) {
+
+    }
+    this.draw = function(context) {
+
+    }
+
+}
+
+function ObjectWithGravity() {
+    this.g = 100;
+
+    this.preUpdate = function(deltaTime) {
+        console.log(deltaTime)
+        this.vy += this.g * deltaTime;
+        this.x += this.vx * deltaTime;
+        this.y += this.vy * deltaTime;
+    }
+}
+ObjectWithGravity.prototype = new Object();
+
+function SphereWithGravity() {
+    this.r = 10;
+
+    this.draw = function(context) {
+        context.fillStyle = "black";
+        context.beginPath();
+        context.arc(this.x, this.y, this.r, 0, 2*Math.PI, true);
+        context.closePath();
+        context.fill();
+    }
+}
+SphereWithGravity.prototype = new ObjectWithGravity();
