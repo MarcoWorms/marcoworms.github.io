3e117e9c0a5e1ed9255a37619491e198e11b8ec9
Marco Guaspari Worms <marcogworms@gmail.com>
2016-05-09 00:33:07 -0300

    Added files via upload

--- defense/c2runtime.js
+++ defense/c2runtime.js
@@ -14925,6 +14925,168 @@ cr.system_object.prototype.loadFromJSON = function (o)
 	};
 })();
 cr.shaders = {};
+cr.shaders["glowhorizontal"] = {src: ["varying mediump vec2 vTex;",
+"uniform mediump sampler2D samplerFront;",
+"uniform mediump float pixelWidth;",
+"uniform mediump float intensity;",
+"void main(void)",
+"{",
+"mediump vec4 sum = vec4(0.0);",
+"mediump float halfPixelWidth = pixelWidth / 2.0;",
+"sum += texture2D(samplerFront, vTex - vec2(pixelWidth * 7.0 + halfPixelWidth, 0.0)) * 0.06;",
+"sum += texture2D(samplerFront, vTex - vec2(pixelWidth * 5.0 + halfPixelWidth, 0.0)) * 0.10;",
+"sum += texture2D(samplerFront, vTex - vec2(pixelWidth * 3.0 + halfPixelWidth, 0.0)) * 0.13;",
+"sum += texture2D(samplerFront, vTex - vec2(pixelWidth * 1.0 + halfPixelWidth, 0.0)) * 0.16;",
+"mediump vec4 front = texture2D(samplerFront, vTex);",
+"sum += front * 0.10;",
+"sum += texture2D(samplerFront, vTex + vec2(pixelWidth * 1.0 + halfPixelWidth, 0.0)) * 0.16;",
+"sum += texture2D(samplerFront, vTex + vec2(pixelWidth * 3.0 + halfPixelWidth, 0.0)) * 0.13;",
+"sum += texture2D(samplerFront, vTex + vec2(pixelWidth * 5.0 + halfPixelWidth, 0.0)) * 0.10;",
+"sum += texture2D(samplerFront, vTex + vec2(pixelWidth * 7.0 + halfPixelWidth, 0.0)) * 0.06;",
+"gl_FragColor = mix(front, max(front, sum), intensity);",
+"}"
+].join("\n"),
+	extendBoxHorizontal: 8,
+	extendBoxVertical: 0,
+	crossSampling: false,
+	preservesOpaqueness: false,
+	animated: false,
+	parameters: [["intensity", 0, 1]] }
+cr.shaders["glowvertical"] = {src: ["varying mediump vec2 vTex;",
+"uniform mediump sampler2D samplerFront;",
+"uniform mediump float pixelHeight;",
+"uniform mediump float intensity;",
+"void main(void)",
+"{",
+"mediump vec4 sum = vec4(0.0);",
+"mediump float halfPixelHeight = pixelHeight / 2.0;",
+"sum += texture2D(samplerFront, vTex - vec2(0.0, pixelHeight * 7.0 + halfPixelHeight)) * 0.06;",
+"sum += texture2D(samplerFront, vTex - vec2(0.0, pixelHeight * 5.0 + halfPixelHeight)) * 0.10;",
+"sum += texture2D(samplerFront, vTex - vec2(0.0, pixelHeight * 3.0 + halfPixelHeight)) * 0.13;",
+"sum += texture2D(samplerFront, vTex - vec2(0.0, pixelHeight * 1.0 + halfPixelHeight)) * 0.16;",
+"mediump vec4 front = texture2D(samplerFront, vTex);",
+"sum += front * 0.10;",
+"sum += texture2D(samplerFront, vTex + vec2(0.0, pixelHeight * 1.0 + halfPixelHeight)) * 0.16;",
+"sum += texture2D(samplerFront, vTex + vec2(0.0, pixelHeight * 3.0 + halfPixelHeight)) * 0.13;",
+"sum += texture2D(samplerFront, vTex + vec2(0.0, pixelHeight * 5.0 + halfPixelHeight)) * 0.10;",
+"sum += texture2D(samplerFront, vTex + vec2(0.0, pixelHeight * 7.0 + halfPixelHeight)) * 0.06;",
+"gl_FragColor = mix(front, max(front, sum), intensity);",
+"}"
+].join("\n"),
+	extendBoxHorizontal: 0,
+	extendBoxVertical: 8,
+	crossSampling: false,
+	preservesOpaqueness: false,
+	animated: false,
+	parameters: [["intensity", 0, 1]] }
+cr.shaders["oilpainting"] = {src: ["#ifdef GL_FRAGMENT_PRECISION_HIGH",
+"#define highmedp highp",
+"#else",
+"#define highmedp mediump",
+"#endif",
+"varying mediump vec2 vTex;",
+"uniform lowp sampler2D samplerFront;",
+"precision highmedp float;",
+"uniform float pixelWidth;",
+"uniform float pixelHeight;",
+"void main (void)",
+"{",
+"const int radius = 3;",
+"vec2 src_size = vec2(1.0 / pixelWidth, 1.0 / pixelHeight);",
+"lowp float alpha = texture2D(samplerFront, vTex).a;",
+"vec2 uv = vTex;",
+"float n = float((radius + 1) * (radius + 1));",
+"vec3 m[4];",
+"vec3 s[4];",
+"for (int k = 0; k < 4; ++k) {",
+"m[k] = vec3(0.0);",
+"s[k] = vec3(0.0);",
+"}",
+"for (int j = -radius; j <= 0; ++j)  {",
+"for (int i = -radius; i <= 0; ++i)  {",
+"vec3 c = texture2D(samplerFront, uv + vec2(i,j) / src_size).rgb;",
+"m[0] += c;",
+"s[0] += c * c;",
+"}",
+"}",
+"for (int j = -radius; j <= 0; ++j)  {",
+"for (int i = 0; i <= radius; ++i)  {",
+"vec3 c = texture2D(samplerFront, uv + vec2(i,j) / src_size).rgb;",
+"m[1] += c;",
+"s[1] += c * c;",
+"}",
+"}",
+"for (int j = 0; j <= radius; ++j)  {",
+"for (int i = 0; i <= radius; ++i)  {",
+"vec3 c = texture2D(samplerFront, uv + vec2(i,j) / src_size).rgb;",
+"m[2] += c;",
+"s[2] += c * c;",
+"}",
+"}",
+"for (int j = 0; j <= radius; ++j)  {",
+"for (int i = -radius; i <= 0; ++i)  {",
+"vec3 c = texture2D(samplerFront, uv + vec2(i,j) / src_size).rgb;",
+"m[3] += c;",
+"s[3] += c * c;",
+"}",
+"}",
+"float min_sigma2 = 1e+2;",
+"for (int k = 0; k < 4; ++k) {",
+"m[k] /= n;",
+"s[k] = abs(s[k] / n - m[k] * m[k]);",
+"float sigma2 = s[k].r + s[k].g + s[k].b;",
+"if (sigma2 < min_sigma2) {",
+"min_sigma2 = sigma2;",
+"gl_FragColor = vec4(m[k], alpha);",
+"}",
+"}",
+"}"
+].join("\n"),
+	extendBoxHorizontal: 0,
+	extendBoxVertical: 0,
+	crossSampling: false,
+	preservesOpaqueness: true,
+	animated: false,
+	parameters: [] }
+cr.shaders["toon"] = {src: ["#ifdef GL_FRAGMENT_PRECISION_HIGH",
+"#define highmedp highp",
+"#else",
+"#define highmedp mediump",
+"#endif",
+"varying mediump vec2 vTex;",
+"uniform lowp sampler2D samplerFront;",
+"precision highmedp float;",
+"uniform highmedp float pixelWidth;",
+"uniform highmedp float pixelHeight;",
+"uniform highmedp float threshold;",
+"uniform highmedp float quantizationLevels;",
+"void main(void)",
+"{",
+"vec4 front = texture2D(samplerFront, vTex);",
+"float dx = pixelWidth;",
+"float dy = pixelHeight;",
+"float bottomLeftIntensity = texture2D(samplerFront, vTex + vec2(-dx, dy)).r;",
+"float topRightIntensity = texture2D(samplerFront, vTex + vec2(dx, -dy)).r;",
+"float topLeftIntensity = texture2D(samplerFront, vTex + vec2(-dx, -dy)).r;",
+"float bottomRightIntensity = texture2D(samplerFront, vTex + vec2(dx, dy)).r;",
+"float leftIntensity = texture2D(samplerFront, vTex + vec2(-dx, 0.0)).r;",
+"float rightIntensity = texture2D(samplerFront, vTex + vec2(dx, 0.0)).r;",
+"float bottomIntensity = texture2D(samplerFront, vTex + vec2(0.0, dy)).r;",
+"float topIntensity = texture2D(samplerFront, vTex + vec2(0.0, -dy)).r;",
+"float h = -topLeftIntensity - 2.0 * topIntensity - topRightIntensity + bottomLeftIntensity + 2.0 * bottomIntensity + bottomRightIntensity;",
+"float v = -bottomLeftIntensity - 2.0 * leftIntensity - topLeftIntensity + bottomRightIntensity + 2.0 * rightIntensity + topRightIntensity;",
+"float mag = length(vec2(h, v));",
+"vec3 posterized = floor((front.rgb * (quantizationLevels-1.0)) + 0.5) / (quantizationLevels-1.0);",
+"float thresholdTest = 1.0 - step(threshold, mag);",
+"gl_FragColor = vec4(posterized * thresholdTest, front.a);",
+"}"
+].join("\n"),
+	extendBoxHorizontal: 0,
+	extendBoxVertical: 0,
+	crossSampling: false,
+	preservesOpaqueness: true,
+	animated: false,
+	parameters: [["threshold", 0, 1], ["quantizationLevels", 0, 0]] }
 ;
 ;
 cr.plugins_.DialogBox = function(runtime)
@@ -17870,15 +18032,338 @@ cr.behaviors.Bullet = function(runtime)
 	};
 	behaviorProto.exps = new Exps();
 }());
+;
+;
+cr.behaviors.Fade = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var behaviorProto = cr.behaviors.Fade.prototype;
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	var behtypeProto = behaviorProto.Type.prototype;
+	behtypeProto.onCreate = function()
+	{
+	};
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	var behinstProto = behaviorProto.Instance.prototype;
+	behinstProto.onCreate = function()
+	{
+		this.activeAtStart = this.properties[0] === 1;
+		this.setMaxOpacity = false;					// used to retrieve maxOpacity once in first 'Start fade' action if initially inactive
+		this.fadeInTime = this.properties[1];
+		this.waitTime = this.properties[2];
+		this.fadeOutTime = this.properties[3];
+		this.destroy = this.properties[4];			// 0 = no, 1 = after fade out
+		this.stage = this.activeAtStart ? 0 : 3;		// 0 = fade in, 1 = wait, 2 = fade out, 3 = done
+		if (this.recycled)
+			this.stageTime.reset();
+		else
+			this.stageTime = new cr.KahanAdder();
+		this.maxOpacity = (this.inst.opacity ? this.inst.opacity : 1.0);
+		if (this.activeAtStart)
+		{
+			if (this.fadeInTime === 0)
+			{
+				this.stage = 1;
+				if (this.waitTime === 0)
+					this.stage = 2;
+			}
+			else
+			{
+				this.inst.opacity = 0;
+				this.runtime.redraw = true;
+			}
+		}
+	};
+	behinstProto.saveToJSON = function ()
+	{
+		return {
+			"fit": this.fadeInTime,
+			"wt": this.waitTime,
+			"fot": this.fadeOutTime,
+			"s": this.stage,
+			"st": this.stageTime.sum,
+			"mo": this.maxOpacity,
+		};
+	};
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.fadeInTime = o["fit"];
+		this.waitTime = o["wt"];
+		this.fadeOutTime = o["fot"];
+		this.stage = o["s"];
+		this.stageTime.reset();
+		this.stageTime.sum = o["st"];
+		this.maxOpacity = o["mo"];
+	};
+	behinstProto.tick = function ()
+	{
+		this.stageTime.add(this.runtime.getDt(this.inst));
+		if (this.stage === 0)
+		{
+			this.inst.opacity = (this.stageTime.sum / this.fadeInTime) * this.maxOpacity;
+			this.runtime.redraw = true;
+			if (this.inst.opacity >= this.maxOpacity)
+			{
+				this.inst.opacity = this.maxOpacity;
+				this.stage = 1;	// wait stage
+				this.stageTime.reset();
+				this.runtime.trigger(cr.behaviors.Fade.prototype.cnds.OnFadeInEnd, this.inst);
+			}
+		}
+		if (this.stage === 1)
+		{
+			if (this.stageTime.sum >= this.waitTime)
+			{
+				this.stage = 2;	// fade out stage
+				this.stageTime.reset();
+				this.runtime.trigger(cr.behaviors.Fade.prototype.cnds.OnWaitEnd, this.inst);
+			}
+		}
+		if (this.stage === 2)
+		{
+			if (this.fadeOutTime !== 0)
+			{
+				this.inst.opacity = this.maxOpacity - ((this.stageTime.sum / this.fadeOutTime) * this.maxOpacity);
+				this.runtime.redraw = true;
+				if (this.inst.opacity < 0)
+				{
+					this.inst.opacity = 0;
+					this.stage = 3;	// done
+					this.stageTime.reset();
+					this.runtime.trigger(cr.behaviors.Fade.prototype.cnds.OnFadeOutEnd, this.inst);
+					if (this.destroy === 1)
+						this.runtime.DestroyInstance(this.inst);
+				}
+			}
+		}
+	};
+	behinstProto.doStart = function ()
+	{
+		this.stage = 0;
+		this.stageTime.reset();
+		if (this.fadeInTime === 0)
+		{
+			this.stage = 1;
+			if (this.waitTime === 0)
+				this.stage = 2;
+		}
+		else
+		{
+			this.inst.opacity = 0;
+			this.runtime.redraw = true;
+		}
+	};
+	function Cnds() {};
+	Cnds.prototype.OnFadeOutEnd = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnFadeInEnd = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnWaitEnd = function ()
+	{
+		return true;
+	};
+	behaviorProto.cnds = new Cnds();
+	function Acts() {};
+	Acts.prototype.StartFade = function ()
+	{
+		if (!this.activeAtStart && !this.setMaxOpacity)
+		{
+			this.maxOpacity = (this.inst.opacity ? this.inst.opacity : 1.0);
+			this.setMaxOpacity = true;
+		}
+		if (this.stage === 3)
+			this.doStart();
+	};
+	Acts.prototype.RestartFade = function ()
+	{
+		this.doStart();
+	};
+	Acts.prototype.SetFadeInTime = function (t)
+	{
+		if (t < 0)
+			t = 0;
+		this.fadeInTime = t;
+	};
+	Acts.prototype.SetWaitTime = function (t)
+	{
+		if (t < 0)
+			t = 0;
+		this.waitTime = t;
+	};
+	Acts.prototype.SetFadeOutTime = function (t)
+	{
+		if (t < 0)
+			t = 0;
+		this.fadeOutTime = t;
+	};
+	behaviorProto.acts = new Acts();
+	function Exps() {};
+	Exps.prototype.FadeInTime = function (ret)
+	{
+		ret.set_float(this.fadeInTime);
+	};
+	Exps.prototype.WaitTime = function (ret)
+	{
+		ret.set_float(this.waitTime);
+	};
+	Exps.prototype.FadeOutTime = function (ret)
+	{
+		ret.set_float(this.fadeOutTime);
+	};
+	behaviorProto.exps = new Exps();
+}());
+;
+;
+cr.behaviors.Rotate = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rotate.prototype;
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	var behtypeProto = behaviorProto.Type.prototype;
+	behtypeProto.onCreate = function()
+	{
+	};
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	var behinstProto = behaviorProto.Instance.prototype;
+	behinstProto.onCreate = function()
+	{
+		this.speed = cr.to_radians(this.properties[0]);
+		this.acc = cr.to_radians(this.properties[1]);
+	};
+	behinstProto.saveToJSON = function ()
+	{
+		return {
+			"speed": this.speed,
+			"acc": this.acc
+		};
+	};
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.speed = o["speed"];
+		this.acc = o["acc"];
+	};
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		if (dt === 0)
+			return;
+		if (this.acc !== 0)
+			this.speed += this.acc * dt;
+		if (this.speed !== 0)
+		{
+			this.inst.angle = cr.clamp_angle(this.inst.angle + this.speed * dt);
+			this.inst.set_bbox_changed();
+		}
+	};
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	function Acts() {};
+	Acts.prototype.SetSpeed = function (s)
+	{
+		this.speed = cr.to_radians(s);
+	};
+	Acts.prototype.SetAcceleration = function (a)
+	{
+		this.acc = cr.to_radians(a);
+	};
+	behaviorProto.acts = new Acts();
+	function Exps() {};
+	Exps.prototype.Speed = function (ret)
+	{
+		ret.set_float(cr.to_degrees(this.speed));
+	};
+	Exps.prototype.Acceleration = function (ret)
+	{
+		ret.set_float(cr.to_degrees(this.acc));
+	};
+	behaviorProto.exps = new Exps();
+}());
+;
+;
+cr.behaviors.destroy = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var behaviorProto = cr.behaviors.destroy.prototype;
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	var behtypeProto = behaviorProto.Type.prototype;
+	behtypeProto.onCreate = function()
+	{
+	};
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	var behinstProto = behaviorProto.Instance.prototype;
+	behinstProto.onCreate = function()
+	{
+	};
+	behinstProto.tick = function ()
+	{
+		this.inst.update_bbox();
+		var bbox = this.inst.bbox;
+		var layout = this.inst.layer.layout;
+		if (bbox.right < 0 || bbox.bottom < 0 || bbox.left > layout.width || bbox.top > layout.height)
+			this.runtime.DestroyInstance(this.inst);
+	};
+}());
 cr.getObjectRefTable = function () { return [
 	cr.plugins_.DialogBox,
 	cr.plugins_.Sprite,
 	cr.plugins_.TiledBg,
 	cr.plugins_.Touch,
 	cr.behaviors.Bullet,
+	cr.behaviors.destroy,
+	cr.behaviors.Fade,
+	cr.behaviors.Rotate,
 	cr.system_object.prototype.cnds.OnLayoutStart,
-	cr.system_object.prototype.cnds.ForEachOrdered,
+	cr.behaviors.Bullet.prototype.acts.SetAngleOfMotion,
+	cr.system_object.prototype.exps.angle,
+	cr.plugins_.Sprite.prototype.exps.X,
 	cr.plugins_.Sprite.prototype.exps.Y,
+	cr.system_object.prototype.cnds.ForEachOrdered,
 	cr.plugins_.Sprite.prototype.acts.MoveToBottom,
 	cr.plugins_.Sprite.prototype.acts.SetAnimFrame,
 	cr.system_object.prototype.exps.floor,
@@ -17887,20 +18372,22 @@ cr.getObjectRefTable = function () { return [
 	cr.system_object.prototype.cnds.Compare,
 	cr.plugins_.Sprite.prototype.acts.SetMirrored,
 	cr.system_object.prototype.cnds.Every,
-	cr.system_object.prototype.cnds.PickRandom,
 	cr.system_object.prototype.acts.CreateObject,
-	cr.plugins_.Sprite.prototype.exps.X,
 	cr.plugins_.Sprite.prototype.exps.Width,
 	cr.plugins_.Sprite.prototype.exps.Height,
-	cr.behaviors.Bullet.prototype.acts.SetAngleOfMotion,
-	cr.system_object.prototype.exps.angle,
 	cr.plugins_.Touch.prototype.cnds.OnTouchStart,
 	cr.plugins_.Sprite.prototype.exps.ImagePointX,
 	cr.plugins_.Sprite.prototype.exps.ImagePointY,
 	cr.plugins_.Touch.prototype.exps.X,
 	cr.plugins_.Touch.prototype.exps.Y,
+	cr.plugins_.Sprite.prototype.acts.SetAngle,
 	cr.plugins_.Sprite.prototype.cnds.OnCollision,
+	cr.behaviors.Bullet.prototype.exps.AngleOfMotion,
+	cr.behaviors.Bullet.prototype.acts.SetSpeed,
+	cr.behaviors.Bullet.prototype.exps.Speed,
 	cr.plugins_.Sprite.prototype.acts.Destroy,
 	cr.plugins_.Sprite.prototype.acts.SubInstanceVar,
-	cr.plugins_.Sprite.prototype.cnds.CompareInstanceVar
+	cr.plugins_.Sprite.prototype.cnds.CompareInstanceVar,
+	cr.system_object.prototype.cnds.EveryTick,
+	cr.system_object.prototype.exps.dt
 ];};
