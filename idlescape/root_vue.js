04b3b28e707c3f78290b513937cfa5fa690c9b10
Marco Guaspari Worms <marcogworms@gmail.com>
2016-05-21 23:34:05 -0300

    update from source repo
    source https://github.com/MarcoWorms/idleScape

--- idlescape/root_vue.js
+++ idlescape/root_vue.js
@@ -3,6 +3,8 @@
 Vue.component('cityselector', idlescape.vues.city_selector)
 Vue.component('cityexplorer', idlescape.vues.city_explorer)
 Vue.component('bank', idlescape.vues.bank)
+Vue.component('mainpopup', idlescape.vues.main_popup)
+Vue.component('skilling-spot-mechanic', idlescape.vues.skilling_spot_mechanic)
 
 idlescape.root_vue = new Vue({
   el: '#root_vue',
@@ -17,7 +19,13 @@ idlescape.root_vue = new Vue({
   }
 })
 
+idlescape.bank = root_vue.bank
+
 idlescape.root_vue.$on('city changed', function (selected_city) {
   this.selected_city = selected_city
   console.log('city changed', idlescape.root_vue.selected_city)
+})
+
+idlescape.root_vue.$on('open_popup', function (...args) {
+  this.$children[1].$children[0].open_spot(...args)
 })
---
+++ idlescape/vue_components/main_popup.js
@@ -0,0 +1,39 @@
+idlescape.vues.main_popup = Vue.extend({
+  template: `
+  <div v-if="visible" id="main_popup" transition="fade">
+    <div @click="hide" class="close_button_container">
+      <p class="close_button">X</p>
+    </div>
+
+    <div id="image_container">
+      <img :src="get_spot_img()"></img>
+    </div>
+    
+    <h1 class="popup_title">{{ spot.name }}</h1>
+
+    <div class="popup_content_container">
+      <skilling-spot-mechanic></skilling-spot-mechanic>
+    </div>
+  </div>
+`,
+  data: function() {
+    return {
+      visible: false,
+      template_type: 'skilling',
+      spot: {}
+    }
+  },
+  props: ['location'],
+  methods: {
+    hide: function () {
+      this.visible = false
+    },
+    open_spot: function (template_type, spot_uid) {
+      this.spot = idlescape.models.spots[template_type][spot_uid];
+      this.visible = true;
+    },
+    get_spot_img: function () {
+      return './imgs/large_icons/' + Object.keys(this.spot.required_levels)[0] + '.png'
+    }
+  }
+})
