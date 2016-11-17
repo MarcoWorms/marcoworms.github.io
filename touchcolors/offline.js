92fc08c306c2f901f9ba598e927f6aed7d5f63fa
Marco Guaspari Worms <marcogworms@gmail.com>
2016-11-17 00:44:22 -0200

    fix bad colors

--- touchcolors/offline.js
+++ touchcolors/offline.js
@@ -1,11 +1,13 @@
 ﻿{
-	"version": 1479186437,
+	"version": 1479350594,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"images/player-sheet0.png",
 		"images/bullet-sheet0.png",
+		"images/bullet-sheet1.png",
+		"images/bullet-sheet2.png",
 		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
