c73803a3124c4e3f9275d6a1ea1e498cb3a01421
Marco Guaspari Worms <marcogworms@gmail.com>
2017-02-26 15:50:34 -0300

    Add files via upload

---
+++ bullet/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1488134737,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player-sheet0.png",
+		"images/playerbody-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/particles.png",
+		"images/stars-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/pew.m4a",
+		"media/pew.ogg",
+		"media/tum.m4a",
+		"media/tum.ogg",
+		"media/boom.m4a",
+		"media/boom.ogg",
+		"media/stardie.m4a",
+		"media/stardie.ogg",
+		"media/collectstar.m4a",
+		"media/collectstar.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
