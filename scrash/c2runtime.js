45b103054890206b67129e865f46b6c6efc22b37
Marco Guaspari Worms <marcogworms@gmail.com>
2017-11-03 19:36:11 -0200

    scrash: release v0.5.0

--- scrash/c2runtime.js
+++ scrash/c2runtime.js
@@ -21506,6 +21506,15 @@ cr.shaders["glowvertical"] = {
 	animated: false,
 	parameters: [["intensity",0,1]]
 };
+cr.shaders["warpripple"] = {
+	src: "varying mediump vec2 vTex;\nuniform lowp sampler2D samplerFront;\nuniform mediump vec2 srcOriginStart;\nuniform mediump vec2 srcOriginEnd;\nuniform mediump float seconds;\nuniform mediump vec2 pixelSize;\nuniform mediump float freq;\nuniform mediump float amp;\nuniform mediump float speed;\nconst mediump float PI = 3.1415926;\nvoid main(void)\n{\nmediump vec2 srcOriginSize = srcOriginEnd - srcOriginStart;\nmediump vec2 tex = (vTex - srcOriginStart) / srcOriginSize;\ntex = tex * 2.0 - 1.0;\nmediump float d = length(tex);\nmediump float a = atan(tex.y, tex.x);\nd += sin((d * 2.0 * PI) * freq / (pixelSize.x * 750.0) + (seconds * speed)) * amp * (pixelSize.x * 750.0);\ntex.x = cos(a) * d;\ntex.y = sin(a) * d;\ntex = (tex + 1.0) / 2.0;\ntex = clamp(tex, 0.0, 1.0);\ntex = tex * srcOriginSize + srcOriginStart;\ngl_FragColor = texture2D(samplerFront, tex);\n}",
+	extendBoxHorizontal: 50,
+	extendBoxVertical: 50,
+	crossSampling: false,
+	preservesOpaqueness: false,
+	animated: true,
+	parameters: [["freq",0,0],["amp",0,1],["speed",0,0]]
+};
 
 
 // c2runtime/box2d.asm.js
@@ -31277,6 +31286,300 @@ cr.plugins_.Mouse = function(runtime)
 }());
 
 
+// Tiled Background
+// ECMAScript 5 strict mode
+
+;
+;
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.TiledBg = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.TiledBg.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+		if (this.is_family)
+			return;
+		
+		this.texture_img = this.runtime.findWaitingTexture(this.texture_file);
+		
+		if (!this.texture_img)
+		{
+			this.texture_img = new Image();
+			this.texture_img.cr_src = this.texture_file;
+			this.texture_img.cr_filesize = this.texture_filesize;
+			this.texture_img.c2webGL_texture = null;
+			this.runtime.waitForImageLoad(this.texture_img, this.texture_file);
+		}
+		
+		this.pattern = null;
+		this.webGL_texture = null;
+	};
+	
+	typeProto.onLostWebGLContext = function ()
+	{
+		if (this.is_family)
+			return;
+			
+		this.webGL_texture = null;
+	};
+	
+	typeProto.onRestoreWebGLContext = function ()
+	{
+		// No need to create textures if no instances exist, will create on demand
+		if (this.is_family || !this.instances.length)
+			return;
+		
+		if (!this.webGL_texture)
+		{
+			this.webGL_texture = this.runtime.glwrap.loadTexture(this.texture_img, true, this.runtime.linearSampling, this.texture_pixelformat);
+		}
+		
+		var i, len;
+		for (i = 0, len = this.instances.length; i < len; i++)
+			this.instances[i].webGL_texture = this.webGL_texture;
+	};
+	
+	typeProto.loadTextures = function ()
+	{
+		if (this.is_family || this.webGL_texture || !this.runtime.glwrap)
+			return;
+			
+		this.webGL_texture = this.runtime.glwrap.loadTexture(this.texture_img, true, this.runtime.linearSampling, this.texture_pixelformat);
+	};
+	
+	typeProto.unloadTextures = function ()
+	{
+		// Don't release textures if any instances still exist, they are probably using them
+		if (this.is_family || this.instances.length || !this.webGL_texture)
+			return;
+		
+		this.runtime.glwrap.deleteTexture(this.webGL_texture);
+		this.webGL_texture = null;
+	};
+	
+	typeProto.preloadCanvas2D = function (ctx)
+	{
+		// draw to preload, browser should lazy load the texture
+		ctx.drawImage(this.texture_img, 0, 0);
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		this.visible = this.properties[0];									// 0=visible, 1=invisible
+		this.rcTex = new cr.rect(0, 0, 0, 0);
+		
+		this.has_own_texture = false;										// true if a texture loaded in from URL
+		this.texture_img = this.type.texture_img;
+		
+		if (this.runtime.glwrap)
+		{
+			// Create WebGL texture if type doesn't have it yet
+			this.type.loadTextures();
+			
+			this.webGL_texture = this.type.webGL_texture;
+		}
+		else
+		{
+			// Create the pattern if the type doesn't have one yet
+			if (!this.type.pattern)
+				this.type.pattern = this.runtime.ctx.createPattern(this.type.texture_img, "repeat");
+			
+			this.pattern = this.type.pattern;
+		}
+	};
+	
+	instanceProto.afterLoad = function ()
+	{
+		// If has own texture, reset to normal texture since the texture isn't saved with the state
+		this.has_own_texture = false;
+		this.texture_img = this.type.texture_img;
+	};
+	
+	instanceProto.onDestroy = function ()
+	{
+		// WebGL renderer: if we have our own texture (via load from URL), delete it now
+		if (this.runtime.glwrap && this.has_own_texture && this.webGL_texture)
+		{
+			this.runtime.glwrap.deleteTexture(this.webGL_texture);
+			this.webGL_texture = null;
+		}
+	};
+
+	instanceProto.draw = function(ctx)
+	{
+		ctx.globalAlpha = this.opacity;
+			
+		ctx.save();
+		
+		ctx.fillStyle = this.pattern;
+		
+		var myx = this.x;
+		var myy = this.y;
+		
+		if (this.runtime.pixel_rounding)
+		{
+			myx = Math.round(myx);
+			myy = Math.round(myy);
+		}
+		
+		// Patterns tile from the origin no matter where you draw from.
+		// Translate the canvas to align with the draw position, then offset the draw position as well.
+		var drawX = -(this.hotspotX * this.width);
+		var drawY = -(this.hotspotY * this.height);
+		
+		var offX = drawX % this.texture_img.width;
+		var offY = drawY % this.texture_img.height;
+		if (offX < 0)
+			offX += this.texture_img.width;
+		if (offY < 0)
+			offY += this.texture_img.height;
+			
+		ctx.translate(myx, myy);
+		ctx.rotate(this.angle);
+		ctx.translate(offX, offY);
+		
+		ctx.fillRect(drawX - offX,
+					 drawY - offY,
+					 this.width,
+					 this.height);
+		
+		ctx.restore();
+	};
+	
+	instanceProto.drawGL_earlyZPass = function(glw)
+	{
+		this.drawGL(glw);
+	};
+	
+	instanceProto.drawGL = function(glw)
+	{
+		glw.setTexture(this.webGL_texture);
+		glw.setOpacity(this.opacity);
+		
+		var rcTex = this.rcTex;
+		rcTex.right = this.width / this.texture_img.width;
+		rcTex.bottom = this.height / this.texture_img.height;
+		
+		var q = this.bquad;
+		
+		if (this.runtime.pixel_rounding)
+		{
+			var ox = Math.round(this.x) - this.x;
+			var oy = Math.round(this.y) - this.y;
+			
+			glw.quadTex(q.tlx + ox, q.tly + oy, q.trx + ox, q.try_ + oy, q.brx + ox, q.bry + oy, q.blx + ox, q.bly + oy, rcTex);
+		}
+		else
+			glw.quadTex(q.tlx, q.tly, q.trx, q.try_, q.brx, q.bry, q.blx, q.bly, rcTex);
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	Cnds.prototype.OnURLLoaded = function ()
+	{
+		return true;
+	};
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	Acts.prototype.SetEffect = function (effect)
+	{
+		this.blend_mode = effect;
+		this.compositeOp = cr.effectToCompositeOp(effect);
+		cr.setGLBlend(this, effect, this.runtime.gl);
+		this.runtime.redraw = true;
+	};
+	
+	Acts.prototype.LoadURL = function (url_, crossOrigin_)
+	{
+		var img = new Image();
+		var self = this;
+		
+		img.onload = function ()
+		{
+			self.texture_img = img;
+			
+			// WebGL renderer
+			if (self.runtime.glwrap)
+			{
+				// Delete any previous own texture
+				if (self.has_own_texture && self.webGL_texture)
+					self.runtime.glwrap.deleteTexture(self.webGL_texture);
+					
+				self.webGL_texture = self.runtime.glwrap.loadTexture(img, true, self.runtime.linearSampling);
+			}
+			// Canvas2D renderer
+			else
+			{
+				self.pattern = self.runtime.ctx.createPattern(img, "repeat");
+			}
+			
+			self.has_own_texture = true;
+			self.runtime.redraw = true;
+			self.runtime.trigger(cr.plugins_.TiledBg.prototype.cnds.OnURLLoaded, self);
+		};
+		
+		if (url_.substr(0, 5) !== "data:" && crossOrigin_ === 0)
+			img.crossOrigin = "anonymous";
+		
+		// use runtime function to work around WKWebView permissions
+		this.runtime.setImageSrc(img, url_);
+	};
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	Exps.prototype.ImageWidth = function (ret)
+	{
+		ret.set_float(this.texture_img.width);
+	};
+	
+	Exps.prototype.ImageHeight = function (ret)
+	{
+		ret.set_float(this.texture_img.height);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
+
 // Solid
 // ECMAScript 5 strict mode
 
@@ -35817,6 +36120,7 @@ cr.getObjectRefTable = function () {
 		cr.plugins_.AJAX,
 		cr.plugins_.TextBox,
 		cr.plugins_.Mouse,
+		cr.plugins_.TiledBg,
 		cr.plugins_.Keyboard.prototype.cnds.OnKey,
 		cr.system_object.prototype.acts.SetTimescale,
 		cr.system_object.prototype.cnds.Compare,
@@ -35905,7 +36209,6 @@ cr.getObjectRefTable = function () {
 		cr.plugins_.Tilemap.prototype.cnds.CompareTileStateAt,
 		cr.plugins_.Tilemap.prototype.acts.SetTileState,
 		cr.plugins_.Sprite.prototype.acts.SetAnim,
-		cr.system_object.prototype.acts.SetMinimumFramerate,
 		cr.plugins_.Mouse.prototype.acts.SetCursor,
 		cr.plugins_.AJAX.prototype.acts.RequestFile,
 		cr.system_object.prototype.acts.GoToLayout,
