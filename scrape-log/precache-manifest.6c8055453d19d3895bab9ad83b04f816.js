e04c975ae7533951413dfd5db55d1ec24443aad7
Marco Guaspari Worms <marcogworms@gmail.com>
2020-07-22 01:20:22 -0300

    Add files via upload

---
+++ scrape-log/precache-manifest.6c8055453d19d3895bab9ad83b04f816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03bc18d1b4cea4d2c35ce9282255ee1",
+    "url": "/scrape-log/index.html"
+  },
+  {
+    "revision": "926e2b309e6bc2354f3f",
+    "url": "/scrape-log/static/css/main.9040846d.chunk.css"
+  },
+  {
+    "revision": "0e9bc0986a32834066b5",
+    "url": "/scrape-log/static/js/2.e23f0133.chunk.js"
+  },
+  {
+    "revision": "926e2b309e6bc2354f3f",
+    "url": "/scrape-log/static/js/main.b8f77b8c.chunk.js"
+  },
+  {
+    "revision": "b9c37e58dc9d66a96fb2",
+    "url": "/scrape-log/static/js/runtime~main.a179dcbb.js"
+  }
+]);
